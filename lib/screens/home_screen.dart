faa4b6e00076b7bb76b6f24f74d378b078c94bd5
Serageldin <serageldin-el-khodary@informatik.hs-fulda.de>
2019-10-29 16:59:10 +0100

    Beagle

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'HavaneserScreen.dart';
+import 'beagle_screen.dart';
+
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -10,12 +13,30 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          leading: Icon(Icons.airline_seat_legroom_extra),
+        leading: Icon(Icons.airline_seat_legroom_extra),
         title: Text("Home"),
         actions: <Widget>[
           Icon(Icons.local_printshop),
         ],
       ),
+      body: Center(
+        child: Column(children: <Widget>[
+          buildRaisedHunderasseButton(context, "Havaneser", HavaneserScreen()),
+          buildRaisedHunderasseButton(context, "Beagle", BeagleScreen())
+        ]),
+      ),
+    );
+  }
+
+  Widget buildRaisedHunderasseButton(BuildContext context, text, widget) {
+    return RaisedButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => widget),
+        );
+      },
+      child: Text(text),
     );
   }
 }
