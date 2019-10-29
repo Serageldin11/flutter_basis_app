df8f33673e7a5656cccc63dfdc20567b417fdb68
Serageldin <serageldin-el-khodary@informatik.hs-fulda.de>
2019-10-29 16:06:35 +0100

    Verzweiflung

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,6 +8,14 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+          leading: Icon(Icons.airline_seat_legroom_extra),
+        title: Text("Home"),
+        actions: <Widget>[
+          Icon(Icons.local_printshop),
+        ],
+      ),
+    );
   }
 }
